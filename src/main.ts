6fd838d60f1f8d6e59d9f2b683c35b71bee5bc30
Kacper <33208083+demondobf@users.noreply.github.com>
2026-05-16 20:41:58 +0200

    Add favicon light color application and refactor main initialization

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { hideCursorAfterInactivity } from "./interaction/hide-cursor-after-inactivity";
+import { applyFaviconLightColor } from "./lighting/apply-favicon-light-color";
 import { getLightColorFromQuery } from "./lighting/get-light-color-from-query";
 import { registerServiceWorker } from "./pwa/register-service-worker";
 import { createPearlGlassScene } from "./scene/create-pearl-glass-scene";
 
-registerServiceWorker();
-
-const pearlGlass = createPearlGlassScene(document.body, {
-  lightColor: getLightColorFromQuery(),
-});
+const lightColor = getLightColorFromQuery();
+const pearlGlass = createPearlGlassScene(document.body, { lightColor });
 
 pearlGlass.start();
+applyFaviconLightColor(lightColor);
 hideCursorAfterInactivity();
+registerServiceWorker();
 
 addEventListener("resize", pearlGlass.resize);
