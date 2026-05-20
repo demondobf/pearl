433c5a806c80fac61e4f875707e0c961bf6f0d2f
Kacper <33208083+demondobf@users.noreply.github.com>
2026-05-20 19:20:03 +0200

    Remove favicon color

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { hideCursorAfterInactivity } from "./interaction/hide-cursor-after-inactivity";
-import { applyFaviconLightColor } from "./lighting/apply-favicon-light-color";
 import { getLightColorFromQuery } from "./lighting/get-light-color-from-query";
 import { registerServiceWorker } from "./pwa/register-service-worker";
 import { createPearlGlassScene } from "./scene/create-pearl-glass-scene";
@@ -8,7 +7,6 @@ const lightColor = getLightColorFromQuery();
 const pearlGlass = createPearlGlassScene(document.body, { lightColor });
 
 pearlGlass.start();
-applyFaviconLightColor(lightColor);
 hideCursorAfterInactivity();
 registerServiceWorker();
 
